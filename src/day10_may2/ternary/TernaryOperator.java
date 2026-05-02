d11ea3c4b7f4f9d421a1f3bc8131c12a7eaf7f9f
fevzikipici <kipicifevzi@gmail.com>
2026-05-02 11:29:18 -0400

    ternary 1

---
+++ src/day10_may2/ternary/TernaryOperator.java
@@ -0,0 +1,30 @@
+package day10_may2.ternary;
+
+public class TernaryOperator {
+
+    public static void main(String[] args) {
+
+        int age=16;
+
+
+
+
+//        if (age<18){
+//            "Cannot vote";
+//        } else {
+//            "can vote";
+//        }
+
+
+
+        // condition ?  this when condition true : that when condition false;
+
+        String status=age>=18 ? "Can vote" : "cannot vote";
+
+
+    }
+
+
+
+
+}
---
+++ src/day10_may2/ternary/TernaryTwo.java
@@ -0,0 +1,10 @@
+package day10_may2.ternary;
+
+public class TernaryTwo {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
